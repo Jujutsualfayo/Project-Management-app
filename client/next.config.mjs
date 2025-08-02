22edb7e96cbcfe50a6058bd93cbd9d81f14c83b3
Benjamin Otieno <alphaspaam@gmail.com>
2025-08-02 16:58:59 +0300

    update

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+// Recreate __dirname for ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const nextConfig = {
   images: {
@@ -19,4 +25,3 @@ const nextConfig = {
 };
 
 export default nextConfig;
-
