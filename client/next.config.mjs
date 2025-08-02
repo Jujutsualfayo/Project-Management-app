6bdfecdf4dcd502d3fb0dbff23800ea233edb1a3
Benjamin Otieno <alphaspaam@gmail.com>
2025-08-02 15:04:44 +0300

    added nextconfig config

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,15 +1,22 @@
 /** @type {import('next').NextConfig} */
+import path from 'path';
+
 const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "pm-s3-images.s3.us-east-2.amazonaws.com",
-        port: "",
-        pathname: "/**",
-      }
-    ]
-  }
+        protocol: 'https',
+        hostname: 'pm-s3-images.s3.us-east-2.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
 };
 
 export default nextConfig;
+
