e86cd5b547b152cc59da50d7eebc74577dd4147d
Benjamin Otieno <alphaspaam@gmail.com>
2025-08-02 17:22:23 +0300

    init

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import importPlugin from "eslint-plugin-import";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +12,17 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  {
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      // Enforce case-sensitive import paths
+      "import/no-unresolved": ["error", { caseSensitive: true }],
+    },
+  },
 ];
 
 export default eslintConfig;
+
